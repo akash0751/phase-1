2537d632a404187ff3d72c9724bffb2217c36371
akash0751 <akashsofficial05@gmail.com>
2025-03-27 10:45:35 +0530

    Add files via upload

---
+++ Task-2/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Task2 } from './Component/Task2'
+
+function App() {
+  
+
+  return (
+    <>
+      <Task2 />
+    </>
+  )
+}
+
+export default App
---
+++ Task-2/src/Component/Task2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Task2 = () => {
+
+    const name = "Akash S";
+  return (
+    <div>
+        <h2>Hello {name}</h2>
+    </div>
+  )
+}
